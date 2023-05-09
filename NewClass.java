24a8c02303fd7fd7613c3926b260c9d3e597aa79
Justin Jang <jjang@occ.cccd.edu>
2023-05-09 16:42:43 -0700

    added my name to new feature

--- NewClass.java
+++ NewClass.java
@@ -3,6 +3,6 @@ public class NewClass
     public static void main(String[] args)
     {
         System.out.println("New Feature!!!");
-        System.out.println("New Feature Again!!!");
+        System.out.println("New Feature Again, Justin!!!");
     }
 }
