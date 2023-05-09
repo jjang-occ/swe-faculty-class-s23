96e21f14f94b944e283310d23c9380376887187a
jjang-occ <54513815+jjang-occ@users.noreply.github.com>
2023-05-09 16:45:33 -0700

    Feature coolstuff (#1)
    * new feature
    
    * added more features
    
    * added my name to new feature

--- NewClass.java
+++ NewClass.java
@@ -2,5 +2,7 @@ public class NewClass
 {
     public static void main(String[] args)
     {
+        System.out.println("New Feature!!!");
+        System.out.println("New Feature Again, Justin!!!");
     }
 }
